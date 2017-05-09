da8e66fa569bb1c1a8978afd337a12947affea5f
benji <benji.b.ramirez@gmail.com>
2017-05-08 23:16:56 -0700

    added flipping operations (left right, top bottom) for generating more training samples

--- format_images.py
+++ format_images.py
@@ -3,6 +3,7 @@
 
 import os
 from PIL import Image
+import PIL
 #  Image.resize(size, resample=0)
 # iterating through directory files, answer by ghostdog74
 # http://stackoverflow.com/questions/3964681/find-all-files-in-a-directory-with-extension-txt-in-python
@@ -34,6 +35,22 @@ def image_to_greyscale(image_list):
     # print(grey_images)
     return grey_images
 
+def add_flipped_images(images):
+    new_images = images
+    for x in range(len(images)):
+        flipped_image = images[x].transpose(Image.FLIP_LEFT_RIGHT)
+        print(flipped_image)
+        new_images.append(flipped_image)
+    return new_images
+
+def add_top_bot_flip(images):
+    new_images = images
+    for x in range(len(images)):
+        top_bot_flip_image = images[x].transpose(Image.FLIP_TOP_BOTTOM)
+        print(top_bot_flip_image)
+        new_images.append(top_bot_flip_image)
+    return new_images
+
 def save_images(images):
     for x in range(len(images)):
         # print(image.info)
@@ -41,6 +58,6 @@ def save_images(images):
 
 if __name__ == "__main__":
     image_directory = "stewie_griffin_GoogleSearch"
-    save_images(image_to_greyscale(resize_images(get_images(image_directory))))
+    save_images(add_top_bot_flip(add_flipped_images(image_to_greyscale(resize_images(get_images(image_directory))))))
 
 
